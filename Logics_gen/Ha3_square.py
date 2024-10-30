4ab6a1b90ab33b05cde9ffbcf6a0b90e1e9985b3
karthik18k <karthik.b@fisdom.com>
2024-10-30 15:30:31 +0530

    from_HackerRank_day1

---
+++ Logics_gen/Ha3_square.py
@@ -0,0 +1,29 @@
+"""Task
+The provided code stub reads and integer, n, from STDIN. For all non-negative integers i < n, print i-
+Example
+n = 3
+The list of non-negative integers that are less than n = 3 is |0, 1, 2. Print the square of each number on a separate line.
+1
+4
+Input Format
+The first and only line contains the integer, n.
+Constraints
+1≤n n≤ 20
+Output Format
+Print n lines, one corresponding to each i.
+Sample Input O
+5
+Sample Output 
+O
+1
+4
+9
+16"""
+
+if __name__ == '__main__':
+    # n = int(input())
+    n=2
+    if 1<=n<=20:
+        for i in range (0,n): 
+            print (i**2)
+            i+=1
